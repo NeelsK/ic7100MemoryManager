f89139753f295f3e7f0661e5c3bb041f3b20a1c3
NeelsK <nkriek@me.com>
2015-07-29 22:30:34 +0200

    Initial Commit

---
+++ ic7100MemoryManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ic7100MemoryManager
+//
+//  Created by Neels Kriek on 29/07/2015.
+//  Copyright (c) 2015 Neels Kriek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ic7100MemoryManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ic7100MemoryManager
+//
+//  Created by Neels Kriek on 29/07/2015.
+//  Copyright (c) 2015 Neels Kriek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
